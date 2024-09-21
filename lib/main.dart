f25a1a280af7f2461992d70eac88d6630a0aef69
Matheus Vieira <53275513+matheusvieira300@users.noreply.github.com>
2024-09-21 18:10:00 -0300

    Row Alinhado

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,33 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Row( //row é linha, horizontal
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start, //para ir para o ínicio
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, //espaço equilibrado
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-           Container(color: Colors.red, width: 100, height: 100,),//altura e largura
-          Container(color: Colors.blue, width: 50, height: 50,),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.red, width: 100, height: 100,),//altura e largura
+              Container(color: Colors.blue, width: 50, height: 50,),
+            ],
+          ),
+           Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blue, width: 100, height: 100,),//altura e largura
+              Container(color: Colors.red, width: 50, height: 50,),
+            ],
+          ),
+          Row( //linha deixando um do lado do outro espaçado igualmente
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyan, height: 50, width: 50,),
+              Container(color: Colors.pinkAccent, height: 50, width: 50,),
+              Container(color: Colors.purple, height: 50, width: 50,),
+            ],
+          )
         ],
       ), //local principal aonde estará o nosso widget/objeto, pilha
     );
