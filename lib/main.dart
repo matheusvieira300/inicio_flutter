2333dc243720fe8b1be0755ac7531235d905fbc8
Matheus Vieira <matheusgomesvieirabr2017@gmail.com>
2024-10-18 17:15:06 -0300

    feat: StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -13,95 +13,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo', //parametros nomeados
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      title: 'Flutter Demo', //parametros nomeados
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Tarefas"),
+        ), //topo
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task("Aprender Flutter"), //chamando o widget
+            Task("Andar de Bike"),
+            Task("Meditar"),
+          ],
         ),
-        home: Container(
-          //Container sendo um objeto pai para poder mudar a cor de tudo
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: // eixo principal do widget
-                MainAxisAlignment.spaceEvenly, //espaço equilibrado
-            crossAxisAlignment: CrossAxisAlignment.center, // cross horizontal
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(// no container não há o risco de ocorrer o erro 
-//overflowed porque o container só ocupa o espaço do widget não podendo ser infinito
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ), //altura e largura
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                  ), //altura e largura
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                //linha deixando um do lado do outro espaçado igualmente
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    height: 50,
-                    width: 50,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.amber,
-                height: 30,
-                width: 300,
-                child: const Text(
-                  'Diamante Amarelo',
-                  style: TextStyle(
-                    //estilo do texto
-                    color: Colors.black,
-                    fontSize: 28, //tamanho
-                  ),
-                  textAlign: TextAlign.center, //para deixar o texto no centro
-                ),
-              ), //child para escrever dentro de um container
-              ElevatedButton(
-                onPressed: () {
-                  print('Você apertou o Botão');
-                },
-                child: const Text('Aperte o Botão!'),
-              ), // botão, mais função anonima
-            ],
+        floatingActionButton: FloatingActionButton(onPressed: () {}), //função anônima
+      ),
+    );
+  }
+}
+
+// stateless widget, widget é tipo um método
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key}); //construtor
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
           ),
-        ) //local principal aonde estará o nosso widget/objeto, pilha
-        );
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.arrow_drop_up)) // Ícones
+              ],
+            ),
+          )
+        ],
+      ),
+    ); //corpo;
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,87 @@
+// Container(
+//           //Container sendo um objeto pai para poder mudar a cor de tudo
+//           color: Colors.white,
+//           child: Column(
+//             mainAxisAlignment: // eixo principal do widget
+//                 MainAxisAlignment.spaceEvenly, //espaço equilibrado
+//             crossAxisAlignment: CrossAxisAlignment.center, // cross horizontal
+//             children: [
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(// no container não há o risco de ocorrer o erro 
+// //overflowed porque o container só ocupa o espaço do widget não podendo ser infinito
+//                     color: Colors.red,
+//                     width: 100,
+//                     height: 100,
+//                   ), //altura e largura
+//                   Container(
+//                     color: Colors.blue,
+//                     width: 50,
+//                     height: 50,
+//                   ),
+//                 ],
+//               ),
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.blue,
+//                     width: 100,
+//                     height: 100,
+//                   ), //altura e largura
+//                   Container(
+//                     color: Colors.red,
+//                     width: 50,
+//                     height: 50,
+//                   ),
+//                 ],
+//               ),
+//               Row(
+//                 //linha deixando um do lado do outro espaçado igualmente
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.cyan,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                   Container(
+//                     color: Colors.pinkAccent,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                   Container(
+//                     color: Colors.purple,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 color: Colors.amber,
+//                 height: 30,
+//                 width: 300,
+//                 child: const Text(
+//                   'Diamante Amarelo',
+//                   style: TextStyle(
+//                     //estilo do texto
+//                     color: Colors.black,
+//                     fontSize: 28, //tamanho
+//                   ),
+//                   textAlign: TextAlign.center, //para deixar o texto no centro
+//                 ),
+//               ), //child para escrever dentro de um container
+//               ElevatedButton(
+//                 onPressed: () {
+//                   print('Você apertou o Botão');
+//                 },
+//                 child: const Text('Aperte o Botão!'),
+//               ), // botão, mais função anonima
+//             ],
+//           ),
+//         ) //local principal aonde estará o nosso widget/objeto, pilha
+//         );
+//   }
+// }
