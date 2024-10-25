2859c63a568556cfdddfc6d7c97a3d37594f0f8b
Matheus Vieira <matheusgomesvieirabr2017@gmail.com>
2024-10-25 12:11:11 -0300

    feat: Delete

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -35,7 +35,7 @@ class TaskDao {
   }
 
   //conversão de tarefa para um map de tarefas
-  Map<String,dynamic> toMap(Task tarefa){
+  Map<String, dynamic> toMap(Task tarefa) {
     print("Convertendo Tarefa em Map: ");
     final Map<String, dynamic> mapDeTarefas = Map();
     mapDeTarefas[_name] = tarefa.nome;
@@ -82,5 +82,13 @@ class TaskDao {
     return toList(result);
   }
 
-  delete(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {
+    print("Deletando Tarefa: $nomeDaTarefa");
+    final Database bancoDeDados = await getDatabase();
+    return bancoDeDados.delete(
+      _tablename,
+      where: "$_name = ?",
+      whereArgs: [nomeDaTarefa],
+    );
+  }
 }
